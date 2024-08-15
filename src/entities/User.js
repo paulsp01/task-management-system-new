a8e6ef8a0b6136a9990d9b42a36598f1eeedc324
paulsp01 <paulswarnalee@gmail.com>
2024-08-15 13:33:22 -0400

    commit

--- src/entities/User.js
+++ src/entities/User.js
@@ -13,20 +13,20 @@ class User {
 
   @Column({
     unique: true,
-    length: 50, // Set a length constraint if needed
+    length: 50, 
   })
   username;
 
   @Column({
     type: "varchar",
-    length: 255, // Adjust length as needed, but ensure it's sufficient for hashed passwords
+    length: 255, 
   })
   password;
 
   @Column({
     type: "varchar",
-    default: "user", // Default role is 'user'
-    length: 20, // Set a length constraint if needed
+    default: "user", 
+    length: 20, 
   })
   role;
 
